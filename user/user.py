afad28b230b0cb79062fb3a57ebbe7c01ea90032
Jun-Gyu Park <wnsrb3001@gmail.com>
2020-10-18 13:35:47 +0000

    user-api

--- user/user.py
+++ user/user.py
@@ -10,17 +10,18 @@ import hashlib
 from bson.json_util import dumps, loads
 
 app = Flask(__name__)
-api = Api(app, version='1.0', title='User API', description='사용자 관리 REST API 문서')
-ns = api.namespace('user',description='사용자 API 목록')
-app.config.SWAGGER_UI_DOC_EXPANSION = 'list'
 
 #--------------------------------------------
 # 사용자 관리를 위한 API 정의
 #--------------------------------------------
 
+api = Api(app, version='1.0', title='User API', description='사용자 관리 REST API 문서')
+ns = api.namespace('user',description='사용자 API 목록')
+app.config.SWAGGER_UI_DOC_EXPANSION = 'list'
+
 model_users = api.model('users',{
     'userid': fields.String(required=True, description='사용자 ID', help='사용자 ID는 필수'),
-    'username' : fields.String(required=True, description='사용자 이름', help='사용자 이름은 필수'),
+    'username' : fields.String(required=False, description='사용자 이름', help='사용자 이름은 필수'),
     'password' : fields.String(required=True, description='비밀번호', help='비밀번호는 필수'),
 })
 
@@ -33,30 +34,25 @@ user_parser.add_argument('username', required=True, help='사용자 이름')
 @ns.route('/<userid>')
 #@ns.expect(user_parser)
 class User(Resource):
+
+    def get(self,userid):
+        """
+            Get a user
+            GET /users/<userid>
+        """
+
     @ns.expect(model_users)
     def post(self,userid):
         """
-            Create User
+            Edit a user
             POST /users/<userid>
         """
-        args = user_parser.parse_args()
-
-        try:
-            userid = args['userid']
-            password = args['password']
-            username = args['username']
-            message = create_user_mongo(userid,username,password)
-        except KeyError:
-            return {'result': 'ERROR_PARAMETER'}, 500
-
-        result = {'result': message, 'userid':userid, 'username':username}
-        return result, 200
 
 @ns.route('/')
 class UserList(Resource):
     def get(self):
         """
-            Get User Lists
+            Get User List
             GET /users/
         """
         try:
@@ -68,7 +64,7 @@ class UserList(Resource):
     @ns.expect(model_users)
     def post(self):
         """
-            Create User
+            Create a User
             POST /users/
         """
         args = user_parser.parse_args()
