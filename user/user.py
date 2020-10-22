15c5c09a5bc1f73a315916507f893a29af7b6fa2
Jun-Gyu Park <wnsrb3001@gmail.com>
2020-10-22 17:02:04 +0000

    조금 더 깔끔하게 수정

--- user/user.py
+++ user/user.py
@@ -20,148 +20,124 @@ ns = api.namespace('user',description='사용자 API 목록')
 app.config.SWAGGER_UI_DOC_EXPANSION = 'list'
 
 model_users = api.model('users',{
-    'userid': fields.String(required=True, description='사용자 ID', help='사용자 ID는 필수'),
-    'username' : fields.String(required=False, description='사용자 이름', help='사용자 이름은 필수'),
+    'user_id': fields.String(required=True, description='사용자 ID', help='사용자 ID는 필수'),
+    'name' : fields.String(required=False, description='사용자 이름', help='사용자 이름은 필수'),
     'password' : fields.String(required=True, description='비밀번호', help='비밀번호는 필수'),
 })
 
-"""
-user_parser = ns.parser()
-user_parser.add_argument('userid', required=True, help='사용자 ID')
-user_parser.add_argument('password', required=True, help='패스워드')
-user_parser.add_argument('username', required=True, help='사용자 이름')
-"""
-@ns.route('/<userid>')
-#@ns.expect(user_parser)
-class User(Resource):
-
-    def get(self,userid):
-        """
-            Get a user
-            GET /users/<userid>
-        """
-        try:
-            message = get_user__mongo(userid)
-        except KeyError:
-            return {'result':'ERROR_PARAMETER'},400
-
-        if not message:
-            return {'result':'해당 ID로 등록된 사용자가 없습니다.'},400
-        return message,200
-
-    @ns.expect(model_users)
-    def post(self,userid):
-        """
-            Edit a user
-            POST /users/<userid>
-        """
-        try:
-            body = request.get_json()
-            userid = userid
-            password = body['password']
-            username = body['username']
-            message = edit_user_mongo(userid,username,password)
-        except KeyError:
-            return {'result': 'ERROR_PARAMETER'}, 500
-
-        if message:
-            return {'result' : '정상적으로 변경되었습니다.', 'userid':userid, 'username':username},200
-        else:
-            return {'result' : 'ID나 비밀번호를 다시 확인해주세요.'},400
-
 @ns.route('/')
-class UserList(Resource):
-    def get(self):
+class UserResource(Resource):
+
+    def get(self, user_id=None):
         """
-            Get User List
-            GET /users/
+            Get a user resource or list users.
+            GET/HEAD /user
+            GET/HEAD /users/{user_id}
         """
-        try:
-            message = get_user_list_mongo()
-        except KeyError:
-            return {'result':'ERROR_PARAMETER'},500
-        
-        return message,200
+        if user_id is not None:
+            return self.get_user_mongo(user_id)
+        return self.list_users_mongo()
+
+    def get_user_mongo(self, user_id):
+        """
+            Get a user resource.
+            GET/HEAD /v3/users/{user_id}
+        """
+        #** MONGO_DB_PATH **#
+        client = MongoClient('mongodb://localhost:27017/')
+        db = client.user
+        collection = db.userlist
+        users = collection.find({"user_id":user_id},{"_id":0,"user_id":1,"name":1})
+        json_data=loads(dumps(users, ensure_ascii=False))
+        client.close()
+        return json_data
+
+    def list_users_mongo(self):
+        """
+            List users.
+            GET/HEAD /v3/users
+        """
+        #** MONGO_DB_PATH **#
+        client = MongoClient('mongodb://localhost:27017/')
+        db = client.user
+        collection = db.userlist
+        users = collection.find({},{"_id":0,"user_id":1,"name":1})
+        json_data=loads(dumps(users, ensure_ascii=False))
+        client.close()
+        if json_data:
+            return json_data
+        else:
+            return {'result': "등록된 사용자가 없습니다."}
 
     @ns.expect(model_users)
     def post(self):
         """
-            Create a User
-            POST /users/
+            Create a user.
+            POST /users
         """
         try:
             body = request.get_json()
-            userid = body['userid']
+            user_id = body['user_id']
             password = body['password']
-            username = body['username']
-            message = create_user_mongo(userid,username,password)
+            name = body['name']
+            message = self.create_user_mongo(user_id,name,password)
         except KeyError:
-            return {'result': 'ERROR_PARAMETER'}, 500
+            return {'result': 'ERROR_PARAMETER'},400
 
-        result = {'result': message, 'userid':userid, 'username':username}
+        result = {'result': message, 'user_id':user_id, 'name':name}
         return result, 200
 
-def get_user__mongo(userid):
-    #** MONGO_DB_PATH **#
-    client = MongoClient('mongodb://localhost:27017/')
-    db = client.user
-    collection = db.userlist
+    def patch(self, user_id):
+        """
+            Update a user.
+            PATCH /users/{user_id}
+        """
+        try:
+            body = request.get_json()
+            user_id = user_id
+            password = body['password']
+            name = body['name']
+            message = self.edit_user_mongo(user_id,name,password)
+        except KeyError:
+            return {'result': 'ERROR_PARAMETER'},400
 
-    user_info = collection.find({"userid":userid},{"_id":0,"userid":1,"username":1})
-    json_data=loads(dumps(user_info, ensure_ascii=False))
-    client.close()
-    return json_data
+        if message:
+            return {'result' : '정상적으로 변경되었습니다.', 'user_id':user_id, 'name':name},200
+        else:
+            return {'result' : 'ID나 비밀번호를 다시 확인해주세요.'},400
 
+    def create_user_mongo(self, user_id, name, password):
+        hasher = hashlib.sha512()
+        hasher.update(password.encode('utf-8'))
+        #** MONGO_DB_PATH **#
+        client = MongoClient('mongodb://localhost:27017/')
+        db = client.user
+        collection = db.userlist
 
-def get_user_list_mongo():
-    #** MONGO_DB_PATH **#
-    client = MongoClient('mongodb://localhost:27017/')
-    db = client.user
-    collection = db.userlist
+        if self.get_user_mongo(user_id):
+            client.close()
+            return "이미 가입된 ID입니다"
 
-    user_list = collection.find({},{"_id":0,"userid":1,"username":1})
-    json_data=loads(dumps(user_list, ensure_ascii=False))
-    if json_data:
+        collection.insert({'user_id':user_id,'password':hasher.hexdigest(),'name':name})
         client.close()
-        return json_data
-    else:
-        client.close()
-        return {'result': "등록된 사용자가 없습니다."}
+        return "회원가입 완료"
 
+    def edit_user_mongo(self, user_id, name, password):
+        hasher = hashlib.sha512()
+        hasher.update(password.encode('utf-8'))
+        #** MONGO_DB_PATH **#
+        client = MongoClient('mongodb://localhost:27017/')
+        db = client.user
+        collection = db.userlist
+        #users = collection.find({"user_id":user_id,"password":hasher.hexdigest()})
+        users = collection.update({"user_id":user_id,"password":hasher.hexdigest()},{"$set":{"name":name}})
+        json_data=loads(dumps(users, ensure_ascii=False))
+        client.close()
     
-def create_user_mongo(userid,username,password):
-    hasher = hashlib.sha512()
-    hasher.update(password.encode('utf-8'))
-    #** MONGO_DB_PATH **#
-    client = MongoClient('mongodb://localhost:27017/')
-    db = client.user
-    collection = db.userlist
-
-    if collection.find_one({"userid":userid}):
-        client.close()
-        return "이미 가입된 ID입니다"
-
-    collection.insert({'userid':userid,'password':hasher.hexdigest(),'username':username})
-    client.close()
-    return "회원가입 완료"
-
-def edit_user_mongo(userid,username,password):
-    hasher = hashlib.sha512()
-    hasher.update(password.encode('utf-8'))
-    #** MONGO_DB_PATH **#
-    client = MongoClient('mongodb://localhost:27017/')
-    db = client.user
-    collection = db.userlist
-    #user_info = collection.find({"userid":userid,"password":hasher.hexdigest()})
-    user_info = collection.update({"userid":userid,"password":hasher.hexdigest()},{"$set":{"username":username}})
-    json_data=loads(dumps(user_info, ensure_ascii=False))
-    client.close()
-    
-    if json_data['nModified'] :
-        return True
-    else:
-        return False
-
+        if json_data['nModified'] :
+            return True
+        else:
+            return False
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
